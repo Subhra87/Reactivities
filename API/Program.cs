71031cd4393585163a08e11555a7b91a027d2e39
Subhra Kanta <subhrakanta.mshr8@gmail.com>
2023-08-22 12:48:26 +0530

    End of work for semantic css implimented

--- API/Program.cs
+++ API/Program.cs
@@ -1,43 +1,56 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
+internal class Program
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        // Add services to the container.
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+        builder.Services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+            });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseCors("CorsPolicy");
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        using var scope = app.Services.CreateScope();
+        var service = scope.ServiceProvider;
+        try
+        {
+            var context = service.GetRequiredService<DataContext>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedData(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = service.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An Error ocrred while creating the database");
+        }
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var service =scope.ServiceProvider;
-try
-{
- var context = service.GetRequiredService<DataContext>();
- await context.Database.MigrateAsync();
- await Seed.SeedData(context);
-}
-catch (Exception ex)
-{
-    var logger = service.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An Error ocrred while creating the database");
-}
-app.Run();
